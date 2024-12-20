7d52a22476020c3809f0f92981d8dd1aa34b5c3a
Crowdbotics <team@crowdbotics.com>
2024-12-20 17:44:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wadawd-50442",
+  url: "https://wadawd-50442.botics.co",
+  api: "https://wadawd-50442.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
